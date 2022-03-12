82a95dc7a0850aab69112ac2dc82b3cadba35a50
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2022-03-12 21:15:25 +0500

    DataType

---
+++ DataType.java
@@ -0,0 +1,21 @@
+public class DataType {
+    public static void main(String args[]){
+
+        //butun sonlar
+        byte a = 1;
+        short b = 2;
+        int c = 3;
+        long d = 4;
+        int res = a * b * c * 4;
+        System.out.println("javob = " + res);
+        // haqiqiy sonlar
+        float e = 2.1f;
+        double f = 43.5;
+        int res2 = a + (int) e;
+        float res3 = a + e;
+        System.out.println("res2: " + res2 +"\n"+ "res3: " + res3);
+        char g = '1';
+
+        boolean h = false;
+    }
+}
