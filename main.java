bbb558aa0b818a3bd689cc46e0ec9dc67495b27d
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2022-03-12 21:15:08 +0500

    hello world

---
+++ main.java
@@ -0,0 +1,17 @@
+public class main {
+    public static void main(String args[]){
+        int length=15, breadth=25;
+        int area;
+        area = length*breadth;
+
+        // calculating area of the rectangle
+        System.out.println("Area of rectangle is " + area);
+
+        // declared a varibale which will store the square root
+        double Area_sqrt;
+
+        // calculating square root of Area of the rectangle
+        Area_sqrt = Math.sqrt(area);
+        System.out.println("Square root of area is " +Area_sqrt);
+    }
+}
