7f820609f65b421338cad641a7facddca822b9cb
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2022-03-15 07:03:32 +0500

    21 IncDecLesson

---
+++ IncDecLesson.java
@@ -0,0 +1,15 @@
+public class IncDecLesson {
+    public static void main(String[] args) {
+//        int x = 10;
+//        System.out.println(x++); // 10(11)
+//        System.out.println(++x); //12
+//        System.out.println(x--); //12 (11)
+//        System.out.println(--x); // 10
+
+        int n = 15;
+        int m = 15;
+        System.out.println(n++ + ++n);  // 15+17=32
+        System.out.println(m++ + m++);  // 15+16=31
+
+    }
+}
