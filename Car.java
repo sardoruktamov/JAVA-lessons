747389ce98b2d3ec8f42595c9909f2e902e2027c
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2022-03-19 22:30:46 +0500

    3 Car

---
+++ Car.java
@@ -0,0 +1,11 @@
+public class Car {
+    // model
+    // color
+    public void getColor(String color){
+        System.out.println(color);
+    }
+
+    public void getModel(String model) {
+        System.out.println(model);
+    }
+}
