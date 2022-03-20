3611e33e136069001fbbd11242d7b5fc40900e4f
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2022-03-20 19:09:57 +0500

    OOP da qayta kod yozildi

--- Car.java
+++ Car.java
@@ -1,6 +1,16 @@
 public class Car {
     // model
     // color
+
+    //--OOP da----------------------
+    public static void main(String[] args) {
+        ConstructorLesson constructorLesson = new ConstructorLesson(1, "test");
+        ConstructorLesson constructorLesson2 = new ConstructorLesson(2, "test-2");
+        constructorLesson.print();
+        constructorLesson2.print();
+    }
+    //------------------------
+
     public void getColor(String color){
         System.out.println(color);
     }
