c1449e0e62519ab1b5e183eafdb0eb2120cd26d6
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2022-03-20 19:10:51 +0500

    oop-5 ConstructorLesson

---
+++ ConstructorLesson.java
@@ -0,0 +1,25 @@
+public class ConstructorLesson {
+    int id;
+    String name;
+
+    public ConstructorLesson(){
+    }
+
+    // bu constructordan CAR classida foydalanilgan
+    public ConstructorLesson(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+    // bu constructordan CAR classida foydalanilgan
+    public void print() {
+        System.out.println(id + "-" + name);
+    }
+
+    // bu constructordan Student classida foydalanilgan
+    public static void main(String[] args) {
+        Student student1 = new Student(1, "Alisher", "Rishton tumani", 998911576777L);
+        Student student2 = new Student(2, "Sardor", "Margilon shaxar", 998911095144L);
+        student1.print();
+        student2.print();
+    }
+}
