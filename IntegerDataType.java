0b32e7eeeb76e8948b2c3b23bdfc835588a2775d
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2022-03-21 11:15:33 +0500

    change

--- IntegerDataType.java
+++ IntegerDataType.java
@@ -7,21 +7,23 @@ public class IntegerDataType {
         byte number1 = 1;
         System.out.println(number1);
         number1 = 3;
-        System.out.println(number1);
+        //System.out.println(number1);
 
         short number2 = 20;
-        System.out.println(number2);
+        //System.out.println(number2);
         number2 = 40 + 20;
-        System.out.println(number2);
+        //System.out.println(number2);
 
         int million = 1_000_000;
-        System.out.println(million);
+        //System.out.println(million);
         million = million / 20;
-        System.out.println(million);
+        //System.out.println(million);
 
         long bignumber = 1326L;
-        System.out.println(bignumber);
+        //System.out.println(bignumber);
         bignumber = number1 + number2 + million;
-        System.out.println(bignumber);
+        //System.out.println(bignumber);
+
+
     }
 }
